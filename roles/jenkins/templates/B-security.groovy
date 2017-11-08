204582f2bc834a949becd35ee72aa6072f498193
Alex Baptista <alexbaptista@Alex-MacBookPro.lan>
2017-11-08 21:34:12 -0200

    Add install plugins list and security stack as Groovy script

--- roles/jenkins/templates/B-security.groovy
+++ roles/jenkins/templates/B-security.groovy
@@ -1,6 +1,7 @@
 #!groovy
 import hudson.security.*
 import jenkins.model.*
+import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
@@ -10,7 +11,6 @@ users_s = users.collect { it.toString() }
 // Create the admin user account if it doesn't already exist.
 if ("{{ jenkins_admin_username }}" in users_s) {
     println "Admin user already exists - updating password"
-
     def user = hudson.model.User.get('{{ jenkins_admin_username }}');
     def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword('{{ jenkins_admin_password }}')
     user.addProperty(password)
@@ -18,11 +18,17 @@ if ("{{ jenkins_admin_username }}" in users_s) {
 }
 else {
     println "--> creating local admin user"
-
     hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
     instance.setSecurityRealm(hudsonRealm)
 
     def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+    strategy.setAllowAnonymousRead(false)
     instance.setAuthorizationStrategy(strategy)
+    // Disable remote CLI
+    // https://jenkins.io/doc/book/managing/cli/
+    instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
+    // Enable Slave to Master Access
+    // https://wiki.jenkins.io/display/JENKINS/Slave+To+Master+Access+Control
+    instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
     instance.save()
 }
